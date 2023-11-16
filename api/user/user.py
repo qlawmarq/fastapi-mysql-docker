a71f1649b444ea105f32ba47e3ede4bfd1ad014c
Masaki <masaki.yoshiiwa@gmail.com>
2023-11-16 15:11:15 +0900

    feat: add black & flake8 auto format config for vscode

--- api/user/user.py
+++ api/user/user.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException
-from database.query import query_get, query_put, query_update
+from database.query import query_get, query_put
 from .auth import Auth
 from .models import UserUpdateRequestModel
 
@@ -9,10 +9,10 @@ auth_handler = Auth()
 def register_user(user_model: UserUpdateRequestModel):
     user = get_user_by_email(user_model.email)
     if len(user) != 0:
-        raise HTTPException(
-            status_code=409, detail='Email user already exist.')
+        raise HTTPException(status_code=409, detail="Email user already exist.")
     hashed_password = auth_handler.encode_password(user_model.password)
-    query_put("""
+    query_put(
+        """
                 INSERT INTO user (
                     first_name,
                     last_name,
@@ -20,13 +20,13 @@ def register_user(user_model: UserUpdateRequestModel):
                     password_hash
                 ) VALUES (%s,%s,%s,%s)
                 """,
-              (
-                  user_model.first_name,
-                  user_model.last_name,
-                  user_model.email,
-                  hashed_password
-              )
-              )
+        (
+            user_model.first_name,
+            user_model.last_name,
+            user_model.email,
+            hashed_password,
+        ),
+    )
     user = get_user_by_email(user_model.email)
     return user[0]
 
@@ -34,17 +34,18 @@ def register_user(user_model: UserUpdateRequestModel):
 def signin_user(email, password):
     user = get_user_by_email(email)
     if len(user) == 0:
-        print('Invalid email')
-        raise HTTPException(status_code=401, detail='Invalid email')
-    if (not auth_handler.verify_password(password, user[0]['password_hash'])):
-        print('Invalid password')
-        raise HTTPException(status_code=401, detail='Invalid password')
+        print("Invalid email")
+        raise HTTPException(status_code=401, detail="Invalid email")
+    if not auth_handler.verify_password(password, user[0]["password_hash"]):
+        print("Invalid password")
+        raise HTTPException(status_code=401, detail="Invalid password")
     return user[0]
 
 
 def update_user(user_model: UserUpdateRequestModel):
     hashed_password = auth_handler.encode_password(user_model.password)
-    query_put("""
+    query_put(
+        """
             UPDATE user 
                 SET first_name = %s,
                     last_name = %s,
@@ -52,32 +53,36 @@ def update_user(user_model: UserUpdateRequestModel):
                     password_hash = %s 
                 WHERE user.email = %s;
             """,
-              (
-                  user_model.first_name,
-                  user_model.last_name,
-                  user_model.email,
-                  hashed_password,
-                  user_model.email,
-              )
-              )
+        (
+            user_model.first_name,
+            user_model.last_name,
+            user_model.email,
+            hashed_password,
+            user_model.email,
+        ),
+    )
     user = get_user_by_email(user_model.email)
     return user[0]
 
 
 def get_all_users():
-    user = query_get("""
+    user = query_get(
+        """
         SELECT  
             user.id,
             user.first_name,
             user.last_name,
             user.email
         FROM user
-        """, ())
+        """,
+        (),
+    )
     return user
 
 
 def get_user_by_email(email: str):
-    user = query_get("""
+    user = query_get(
+        """
         SELECT 
             user.id,
             user.first_name,
@@ -86,12 +91,15 @@ def get_user_by_email(email: str):
             user.password_hash
         FROM user 
         WHERE email = %s
-        """, (email))
+        """,
+        (email),
+    )
     return user
 
 
 def get_user_by_id(id: int):
-    user = query_get("""
+    user = query_get(
+        """
         SELECT 
             user.id,
             user.first_name,
@@ -99,5 +107,7 @@ def get_user_by_id(id: int):
             user.email,
         FROM user 
         WHERE id = %s
-        """, (id))
+        """,
+        (id),
+    )
     return user
